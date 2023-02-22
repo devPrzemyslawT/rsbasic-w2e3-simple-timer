d98a86f46571ba66c59d979ce58b16802d527ee1
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-02-22 17:04:24 +0100

    set interval function to count timer value

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [timerValue, setTimer] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTimer(prevCount => prevCount + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Current timer value:</p>
+				<p>{timerValue}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
